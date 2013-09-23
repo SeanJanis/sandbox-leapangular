18ea23685e8c11c49a1ecdd605d92818ff5c5902
Sean Janis <spjanis@gmail.com>
2013-09-22 17:43:49 -0700

    adding support to match hands / finger drawing demo

--- public/js/angular/controllers/c_sandbox.js
+++ public/js/angular/controllers/c_sandbox.js
@@ -12,9 +12,19 @@ function SandboxPartialCtrl($scope, $timeout, $http) {
 
     $scope.init = function() {
         _canvas = document.getElementById('canvas-sandbox');
+
+        // Ensure proper aspect ratio
+        _canvas.width = _canvas.clientWidth;
+        _canvas.height = _canvas.clientHeight;
+
         _context = _canvas.getContext('2d');
         _lm = new Leap.Controller();
-        _lmHandler = new LeapMotionHandler({controller: _lm, scope: $scope});
+        _lmHandler = new LeapMotionHandler({
+                controller: _lm,
+                scope: $scope,
+                canvas: _canvas,
+                context: _context
+        });
         _lmHandler.subscribeListeners();
     };
 
